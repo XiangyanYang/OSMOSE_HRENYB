582bda43507831ea4da0614b6b8b0e8f017a4c3b
XiangyanYang <xiangyan.yang@stonybrook.edu>
2025-06-04 17:45:16 -0400

    updated files on 06042025

--- launcher.R
+++ launcher.R
@@ -1,17 +1,24 @@
+# install.packages("~/osmose_4.4.0.9001", repos = NULL, type = "source")
+# library(osmose)
+# packageVersion("osmose")
+
+install.packages("osmose", repo="https://osmose-model.github.io/drat/")
+install.packages("calibrar", repo="https://osmose-model.github.io/drat/")
 library(osmose)
-library(empirical.selectivity)
+library(calibrar)
+# library(empirical.selectivity)
 
 # 0. OSMOSE configuration -------------------------------------------------
 
 version    = "4.4.1"
-javaPath   = "R:/sync/Github/osmose-private-ricardo/inst/java"
+javaPath   = "/gpfs/home/xiangyyang/osmose/inst/java"
 jarFile    = file.path(javaPath, sprintf("osmose-%s-jar-with-dependencies.jar", version))
 
 # 1. Model Configuration --------------------------------------------------
 
-configDir  = "osmose-hrenyb-v4.4.1-reference"
+configDir  = "/gpfs/home/xiangyyang/OSMOSEZ-HRENYB_MINE/osmose-hrenyb-v4.4.1-xiangyan"
 main       = "osmose-hrenyb.R"
-simulation = "reference"
+simulation = "test0604"
 
 configFile = file.path(configDir, main) # path to main configuration file
 outputDir  = file.path(configDir, "output", simulation)
@@ -27,6 +34,8 @@ out = initialize_osmose(input=configFile, file=inifile, output=outputDir,
                   type = "internannual", osmose = jarFile, version=version,
                   append=FALSE)
 
+
+# generate init results for each sp and save
 # dir.create("test_init_final")
 # 
 # sppn = get_species(conf, type="focal")
@@ -45,10 +54,14 @@ out = initialize_osmose(input=configFile, file=inifile, output=outputDir,
 options = "-Xmx3g -Xms1g"
 run_osmose(input = configFile, output = outputDir, osmose = jarFile, version=version, options=options)
 
+
 nyb = read_osmose(path = outputDir, version=version)
+plot(nyb)
 
 # 4. Calibration setup ----------------------------------------------------
 
+setwd("/gpfs/home/xiangyyang/OSMOSEZ-HRENYB_MINE/")
+configFile = "osmose-hrenyb-v4.4.1-xiangyan/osmose-hrenyb.R"
 calibration_path = osmose_calibration_setup(input=configFile, osmose=jarFile, 
                                             type="survey", name = "nyb01", 
                                             control=list(skip_tests=TRUE))
@@ -58,4 +71,4 @@ calibration_path = osmose_calibration_setup(input=configFile, osmose=jarFile,
                                             type="survey", name="nyb01", data_path = "data")
 
 osmose_calibration_test(calibration_path)
-osmose_calibration_test(calibration_path, parallel.only = TRUE)
+#osmose_calibration_test(calibration_path, parallel.only = TRUE)
